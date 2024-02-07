13dbeb6f3c88cf725756d8700f5a05a73f42dc61
SamyShaawat <samy.shaawat@gmail.com>
2024-02-07 11:38:25 +0200

    add create listing api route

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from 'cookie-parser';
-
 dotenv.config();
 
 // Connect to MongoDB
@@ -29,6 +29,8 @@ app.listen(PORT, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
 app.use((error, req, res, next) => {
     const statusCode = error.statusCode || 500;
     const message = error.message || "Internal Server Error";
