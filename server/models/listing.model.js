1003a26bf0bddb553e274c51aa29702b73cb28ef
SamyShaawat <samy.shaawat@gmail.com>
2024-02-15 08:35:58 +0200

    Complete create listing page functionality

--- server/models/listing.model.js
+++ server/models/listing.model.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const listingSchema = new mongoose.Schema({
-    name: {
+    title: {
         type: String,
         required: true,
     },
@@ -13,7 +13,7 @@ const listingSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    regualarPrice: {
+    regularPrice: {
         type: Number,
         required: true,
     },
